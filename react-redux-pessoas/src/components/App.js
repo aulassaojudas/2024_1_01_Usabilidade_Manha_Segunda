e0cdf27eca470936c963df482808adc31122876e
asbonato <asbonato@gmail.com>
2024-05-07 21:33:23 -0300

    redux(react): aula 07/05 - apostila 9 finalizada

---
+++ react-redux-pessoas/src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import PessoaLista from "./PessoaLista"
+import PessoaDetalhe from "./PessoaDetalhe"
+
+const App = () => {
+    return (
+        <div className="grid border border-1 border-400 m-2">
+            <div className="col-6">
+                <PessoaLista />
+            </div>
+            <div className="col-6">
+                <PessoaDetalhe />
+            </div>
+        </div>
+    )
+}
+export default App
